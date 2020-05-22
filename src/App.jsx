5b3dbc1bab5f5e593cbb17162d52264bcfcc0ce2
Jakub Modzelewski <jakubjmodzelewski@gmail.com>
2020-05-22 17:24:05 +0200

    deleted unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import * as $ from "jquery";
-import logo from './logo.svg';
 import './App.css';
-import { authEndpoint, clientId, redirectUri, scopes } from "./config";
 import Player from "./Player";
-import hash from "./hash";
-import SpotifyWebApi from 'spotify-web-api-js';
 import { getToken } from './store/token/selectors';
 import { getQueryParameter } from './utils/getQueryParameter';
 import { getAccessToken } from './utils/getAccessToken';
-import { getAuthorizationCode } from './utils/getAuthorizationCode';
 import { addToken } from './store/token/actions';
 import LandingPage from "./LandingPage"
 
