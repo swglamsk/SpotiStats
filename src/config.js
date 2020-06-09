9becf16e25116cbc952553fb913ab79031ae6156
Jakub Modzelewski <jakubjmodzelewski@gmail.com>
2020-06-09 22:52:07 +0200

    Revert "test"
    This reverts commit 5fe1214dcc67b6f972068da2064611f02c2b067f.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize?";
 export const clientId = "dc9baef164d3457b80831a005a3a4a33";
 export const clientSecret = "e03ef52772524e089caac17932b16789";
-export const redirectUri = "https://spotistats-zpi.netlify.app/";
+export const redirectUri = "http://localhost:3000";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
